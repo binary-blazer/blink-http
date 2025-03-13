27ab67af1afe3aed040a6a082003f2a757794332
Jonas Franke <jonasfranke@sdevs.org>
2025-03-13 19:06:28 +0100

    Remove outdated example files and add new examples for BlinkClient usage

--- example/index2.js
+++
@@ -1,8 +0,0 @@
-import { blink } from '../distribution/index.js';
-
-async function test() {
-  const response = await blink.get('https://jsonplaceholder.typicode.com/posts/1');
-  console.log(await response.json());
-}
-
-test();
